0f428761f373c9f64c509a983308f047489d9860
Roger2kTaylor <roger@skye-nets.com>
2021-08-21 22:40:08 -0400

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "SkyeLab-rg"
+    resource_group_name  = "skyelab-remote-rg"
     storage_account_name = "skyeremotesa01"
     container_name       = "tfstate"
     key                  = "backend.tfstate"
