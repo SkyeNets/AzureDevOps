47a17ca29a6ec1ef5795ba05446c894d0f9a9290
Roger2kTaylor <roger@skye-nets.com>
2021-08-22 02:00:29 -0400

    Updated bcks33

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
     storage_account_name = "skyeremotesa01"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
- #   subscription_id      = "953ffea1-7504-4a80-b6ad-239156bd6baa"
+    subscription_id      = "953ffea1-7504-4a80-b6ad-239156bd6baa"
 
 
   }
