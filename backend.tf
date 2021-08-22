b4815c90d23d1597d795025d69dab210cc7cfc95
Roger2kTaylor <roger@skye-nets.com>
2021-08-21 23:44:48 -0400

    Updated bcks2

--- backend.tf
+++ backend.tf
@@ -3,13 +3,13 @@
 
 terraform {
   backend "azurerm" {
-  /*  resource_group_name  = "skyelab-remote-rg"
+    resource_group_name  = "skyelab-remote-rg"
     storage_account_name = "skyeremotesa01"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    subscription_id = "953ffea1-7504-4a80-b6ad-239156bd6baa" 
-  */
-  
+    subscription_id      = "953ffea1-7504-4a80-b6ad-239156bd6baa"
+
+
   }
 
 }
